4f34edd84dfb062eeb3bc1807517b63afe5e3eab
Welington Galvao <galvao.eu@gmail.com>
2024-12-03 02:02:26 -0300

    refactor: userId Fornecedores by next-auth

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,13 +1,21 @@
 from rest_framework import viewsets
 from .models import Fornecedor
 from .serializers import FornecedorSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class FornecedorViewSet(viewsets.ModelViewSet):
     queryset = Fornecedor.objects.all()
     serializer_class = FornecedorSerializer
 
-    def get_serializer_context(self):
-        context = super().get_serializer_context()
-        context['request'] = self.request
-        return context
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+    #     context['request'] = self.request
+    #     return context
+
+    @action(detail=False, methods=['get'])
+    def list_by_user(self, request, user_id=None):
+        fornecedores = Fornecedor.objects.filter(user_id=user_id)
+        serializer = self.get_serializer(fornecedores, many=True)
+        return Response(serializer.data)
