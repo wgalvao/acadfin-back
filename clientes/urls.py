5a2a864012bc9a626cd9f6de94e6d43d01daf751
Welington Galvao <galvao.eu@gmail.com>
2024-12-03 00:53:59 -0300

    refactor: userId clinte/empresa by next-auth

--- clientes/urls.py
+++ clientes/urls.py
@@ -7,4 +7,6 @@ router.register(r'clientes', ClienteViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('clientes/user/<int:user_id>/',
+         ClienteViewSet.as_view({'get': 'list_by_user'}), name='cliente-list-by-user'),
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets
 from .models import Cliente
 from .serializers import ClienteSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ClienteViewSet(viewsets.ModelViewSet):
@@ -9,3 +11,8 @@ class ClienteViewSet(viewsets.ModelViewSet):
 
     # def perform_create(self, serializer):
     #     serializer.save(user_id=self.request.user)
+    @action(detail=False, methods=['get'])
+    def list_by_user(self, request, user_id=None):
+        clientes = Cliente.objects.filter(user_id=user_id)
+        serializer = self.get_serializer(clientes, many=True)
+        return Response(serializer.data)
