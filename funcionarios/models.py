21b0e0ab9c16913bfe6cc7b2c2b06083fd1d4604
wgalvao <galvao.eu@gmail.com>
2024-12-04 16:29:10 -0300

    feat: add cargo to funcionario

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 from empresas.models import Empresa
-
+from cargos.models import Cargo  # Importe o modelo Cargo
 
 class Funcionario(models.Model):
     id = models.BigAutoField(primary_key=True)
     nome = models.CharField(max_length=50, verbose_name="Nome do Funcionário")
-    estado_civil = models.CharField(
-        max_length=55, verbose_name="Estado Civil", blank=True, null=True)
-    data_nasc = models.DateField(
-        verbose_name="Data de Nascimento", blank=True, null=True)
+    estado_civil = models.CharField(max_length=55, verbose_name="Estado Civil", blank=True, null=True)
+    data_nasc = models.DateField(verbose_name="Data de Nascimento", blank=True, null=True)
     idade = models.IntegerField(verbose_name="Idade", blank=True, null=True)
-    sexo = models.CharField(
-        max_length=1, verbose_name="Sexo", blank=True, null=True)
-    empresa = models.ForeignKey(
-        Empresa, on_delete=models.CASCADE)
+    sexo = models.CharField(max_length=1, verbose_name="Sexo", blank=True, null=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     escolaridade = models.CharField(max_length=55, blank=True, null=True)
     ctps = models.CharField(max_length=30, blank=True, null=True)
     serie = models.CharField(max_length=15, blank=True, null=True)
@@ -32,6 +28,9 @@ class Funcionario(models.Model):
     user_id = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True, auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, null=True, auto_now=True)
+    
+    # Adicione o campo cargo como chave estrangeira
+    cargo = models.ForeignKey(Cargo, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Cargo")
 
     def __str__(self):
         return self.nome
