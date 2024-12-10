ff53574f52a8d5df5bd1b636aad59aa136c0cafd
Welington Galvao <galvao.eu@gmail.com>
2024-12-09 22:55:24 -0300

    feat: salario e meses em funcionario

--- funcionarios/serializers.py
+++ funcionarios/serializers.py
@@ -1,30 +1,27 @@
+# funcionarios/serializers.py
+
 from rest_framework import serializers
 from .models import Funcionario
-import re
+from cargos.models import Cargo  # Importe o modelo Cargo
+
 
 class FuncionarioSerializer(serializers.ModelSerializer):
     empresa_nome = serializers.SerializerMethodField()
-    cargo_nome = serializers.SerializerMethodField()  # Adicione este campo
+    cargo_nome = serializers.SerializerMethodField()
+    salario = serializers.SerializerMethodField()  # Novo campo para o salário
 
     class Meta:
         model = Funcionario
         fields = '__all__'
 
-    def validate_cpf(self, value):
-        # Verifica se o CPF contém apenas números e tem 11 dígitos
-        if not re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$', value):
-            raise serializers.ValidationError("O CPF deve conter exatamente 11 dígitos numéricos.")
-
-        return value
-
-    def validate_email(self, value):
-        if not value:
-            raise serializers.ValidationError("O campo de email é obrigatório.")
-
-        return value
-
     def get_empresa_nome(self, obj):
         return obj.empresa.nome_razao if obj.empresa else None
 
-    def get_cargo_nome(self, obj):  # Adicione este método
+    def get_cargo_nome(self, obj):
         return obj.cargo.cargo if obj.cargo else None
+
+    def get_salario(self, obj):  # Método para obter o salário do cargo
+        if obj.cargo:
+            cargo = Cargo.objects.get(id=obj.cargo.id)
+            return cargo.salario
+        return None
